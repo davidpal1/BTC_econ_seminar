a67c35a1c75a82360ebe9605c7ab40bd8043af08
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-10-27 11:23:17 +0200

    Add files via upload

---
+++ BTC_ziskam_ceny.py
@@ -0,0 +1,25 @@
+import os
+import time
+from datetime import date, timedelta,datetime
+from typing import Dict, Optional
+
+import numpy as np
+import pandas as pd
+from loguru import logger
+from strenum import StrEnum
+"""
+Nacteni minutovych cen BTC
+mezi daty cas_start a cas_end
+"""
+BTC_cena = pd.read_csv('BTC_1min.txt',header=None)
+BTC_cena.columns = ["cas", "open", "high", "low","closed","vol"]
+
+BTC_cena['cas'] = pd.to_datetime(
+                          BTC_cena["cas"],
+                          format="%Y-%m-%d %H:%M:%S")
+
+cas_start=datetime(year=2022,month=9,day=1,hour=0,minute=0,second=0)
+cas_end=datetime(year=2022,month=9,day=1,hour=23,minute=59,second=59)
+
+BTC_dnes=BTC_cena.loc[BTC_cena["cas"] >cas_start]
+BTC_dnes=BTC_dnes.loc[BTC_dnes["cas"] <cas_end]
