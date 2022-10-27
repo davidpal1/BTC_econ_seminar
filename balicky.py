7c1076071c34daf5d5272c0db13e00b49c9c4e37
davidpal1 <99826118+davidpal1@users.noreply.github.com>
2022-10-27 11:41:43 +0200

    Add files via upload
    Potřebné balíčky pro rozjetí celého kódu

---
+++ balicky.py
@@ -0,0 +1,10 @@
+import os
+import time
+from datetime import date, timedelta,datetime
+from typing import Dict, Optional
+
+import numpy as np
+import pandas as pd
+from loguru import logger
+from strenum import StrEnum
+from statsmodels.tsa.seasonal import seasonal_decompose
